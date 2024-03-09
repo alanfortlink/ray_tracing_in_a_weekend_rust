d726423f5f4ef5b1b49e6916f139358d3c4c4ec3
Alan Silva <alanfortlink@gmail.com>
2024-03-09 16:51:25 +0000

    Make it look more like the tutorial

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,15 @@ use crate::sphere::Sphere;
 use crate::vec3::Point3;
 
 fn main() -> std::io::Result<()> {
-    // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width: u32 = 400;
-
-    let camera = Camera::new(aspect_ratio, image_width);
-
     // World
     let mut world: HittableList = HittableList::new();
     world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+
+    // Camera
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width: u32 = 400;
+    let camera = Camera::new(aspect_ratio, image_width);
 
     // Render
     camera.render(&world)?;
